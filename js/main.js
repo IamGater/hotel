094075d73b15a3faea56f035ecb279e7ee18365f
IamGater <116513307+IamGater@users.noreply.github.com>
2023-07-18 16:59:08 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.slider-button-prev',
     },
   });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
